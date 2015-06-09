d805fd8d70aa3049a53306410d59193e74320946
yarasheva <shevchenko.yaroslav@mail.ru>
2015-06-10 00:28:49 +0300

    first comit

---
+++ project/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(688, 295);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(76, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Дата";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(174, 71);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Организация";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(300, 71);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(58, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Город";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(401, 71);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(64, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Страна";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(508, 71);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(83, 17);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "Менеджер";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(642, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "виполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 465);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ project/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+        string grouping = "";
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            grouping = "";
+            if (checkBox1.Checked) grouping += "Дата,";
+            if (checkBox2.Checked) grouping += "Организация,";
+            if (checkBox3.Checked) grouping += "Город,";
+            if (checkBox4.Checked) grouping += "Страна,";
+            if (checkBox5.Checked) grouping += "Менеджер,";
+                try
+                {
+                    using (SqlConnection cn = new SqlConnection())
+                    {
+                       // cn.ConnectionString = @"Data Source=ЯРИК-ПК\MSSQLSERVER1;Initial Catalog=ty;User ID=sa;Password=yaroslavshevchenko18";
+                        string Nameofserver = ConfigurationSettings.AppSettings["Nameofserver"];
+                        string Nameofinstance = ConfigurationSettings.AppSettings["Nameofinstance"];
+                        string Nameofdb = ConfigurationSettings.AppSettings["Nameofdb"];
+                        string login = ConfigurationSettings.AppSettings["login"];
+                        string Password = ConfigurationSettings.AppSettings["Password"];                        
+                        cn.ConnectionString = string.Format("Data Source={0}\\{1};Initial Catalog={2};User ID={3};Password={4}",
+                            Nameofserver, Nameofinstance, Nameofdb, login, Password);
+                        cn.Open();
+                        string sqlQuery;
+                        SqlCommand sqlCmd;
+                        sqlCmd = new SqlCommand();
+                        if (grouping != "")
+                        {
+                            sqlQuery = "DECLARE @sqlCommand varchar(1000) SET @sqlCommand = 'SELECT ' + @columnList + ' FROM Store  group by '+@grouping EXEC (@sqlCommand)";
+                            sqlCmd = new SqlCommand(sqlQuery, cn);
+                            sqlCmd.Parameters.Add("@grouping", SqlDbType.VarChar).Value = grouping.Substring(0, grouping.Length - 1);
+                            //MessageBox.Show(grouping.Substring(0,grouping.Length-1));
+                            sqlCmd.Parameters.Add("@columnList", SqlDbType.VarChar).Value = grouping + "Количиство=Sum(Количиство),Сумма=Sum(Сумма)";
+                        }
+                        else { sqlQuery = "select * from Store"; sqlCmd = new SqlCommand(sqlQuery, cn); }
+                        SqlDataReader reader = sqlCmd.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+                            dataGridView1.DataSource = dt;
+                        }
+                        Console.ReadLine();
+                        cn.Close();
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_Click(sender,e);
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+               
+            
+
+
+    }
+}
